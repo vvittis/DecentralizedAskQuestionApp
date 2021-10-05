9259274be6d336d5999c69a640fbcc21f131aa48
vvittis <vasilisvittis@gmail.com>
2021-10-05 21:02:30 +0300

    farm

---
+++ build/precache-manifest.ab167ad90fdded343995664de9a817be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b315c6ae8738c49afe13ea5cefca6f1d",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "745fa3d512580e747de1",
+    "url": "/main/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8257682f8f66b4f13b4c",
+    "url": "/main/static/js/2.509a12ad.chunk.js"
+  },
+  {
+    "revision": "745fa3d512580e747de1",
+    "url": "/main/static/js/main.0b616b11.chunk.js"
+  },
+  {
+    "revision": "0ba46ded7ead283b76f8",
+    "url": "/main/static/js/runtime-main.c93338bc.js"
+  }
+]);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,29 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = async function(deployer, networks, accounts) {  // using async for step by step
+
+  // Deploy Mock Dai Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy Mock Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+
+  // deployer.deploy(TokenFarm(dappToken,daiToken));
+  // Deploy Token Farm
+  await deployer.deploy(TokenFarm,dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  // Liquidity Pool -> Add all DappToken into TokenFarm in order to distribute them to all users
+
+  await dappToken.transfer(tokenFarm.address,'1000000000000000000000000')
+
+  // Transfer 100 Mock Dai tokens to investor
+
+  await daiToken.transfer(accounts[1], '100000000000000000000' )
+
 };
