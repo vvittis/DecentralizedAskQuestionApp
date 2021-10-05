7c4b02437aff737a2c88404cc0dd32c780f17e3b
vvittis <vasilisvittis@gmail.com>
2021-10-05 21:04:23 +0300

    Updates

---
+++ precache-manifest.08707879e2dd2daa6dbd32defc5d521c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ef5489bc9e5afde8cc935b37752e5e",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "01510355046022764fba",
+    "url": "/main/static/css/2.d7892326.chunk.css"
+  },
+  {
+    "revision": "c503f691b8ddb79dcf7b",
+    "url": "/main/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "01510355046022764fba",
+    "url": "/main/static/js/2.e7c569ad.chunk.js"
+  },
+  {
+    "revision": "c503f691b8ddb79dcf7b",
+    "url": "/main/static/js/main.ba6ae0fc.chunk.js"
+  },
+  {
+    "revision": "0ba46ded7ead283b76f8",
+    "url": "/main/static/js/runtime-main.c93338bc.js"
+  }
+]);
