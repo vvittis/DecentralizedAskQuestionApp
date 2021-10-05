c7983fc68bd47c508c92878a8291719ac7c8a9fd
vvittis <vasilisvittis@gmail.com>
2021-10-05 21:36:49 +0300

    account1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,29 +1,23 @@
-const TokenFarm = artifacts.require("TokenFarm");
-const DaiToken = artifacts.require("DaiToken");
-const DappToken = artifacts.require("DappToken");
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = async function(deployer, networks, accounts) {  // using async for step by step
-
-  // Deploy Mock Dai Token
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
   await deployer.deploy(DaiToken)
   const daiToken = await DaiToken.deployed()
 
-  // Deploy Mock Dapp Token
+  // Deploy Dapp Token
   await deployer.deploy(DappToken)
   const dappToken = await DappToken.deployed()
 
-
-  // deployer.deploy(TokenFarm(dappToken,daiToken));
-  // Deploy Token Farm
-  await deployer.deploy(TokenFarm,dappToken.address, daiToken.address)
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
   const tokenFarm = await TokenFarm.deployed()
 
-  // Liquidity Pool -> Add all DappToken into TokenFarm in order to distribute them to all users
+  // Transfer all tokens to TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
 
-  await dappToken.transfer(tokenFarm.address,'1000000000000000000000000')
-
-  // Transfer 100 Mock Dai tokens to investor
-
-  await daiToken.transfer(accounts[1], '100000000000000000000' )
-
-};
+  // Transfer 100 Mock DAI tokens to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
+}
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,94 +1,75 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.5.0;
+
 import "./DappToken.sol";
 import "./DaiToken.sol";
 
-
 contract TokenFarm {
+	string public name = "Dapp Token Farm";
+	address public owner;
+	DappToken public dappToken;
+	DaiToken public daiToken;
 
-	string public name = "Dapp Token Farm"; // state variable -> it will be stored in the blockchain
-	DappToken public dappToken;             // address for Dapp Token
-	DaiToken  public daiToken;              // address for Dai Token
-	address   public owner;
 	address[] public stakers;
 	mapping(address => uint) public stakingBalance;
 	mapping(address => bool) public hasStaked;
 	mapping(address => bool) public isStaking;
 
-	constructor (DappToken _dappToken, DaiToken _daiToken) public {
+	constructor(DappToken _dappToken, DaiToken _daiToken) public {
 		dappToken = _dappToken;
-		daiToken  = _daiToken;
+		daiToken = _daiToken;
 		owner = msg.sender;
 	}
 
+	function stakeTokens(uint _amount) public {
+		// Require amount greater than 0
+		require(_amount > 0, "amount cannot be 0");
 
-	// 1. User Stakes Tokens (Deposit)
-	/*
-		We essentially want to tranfer Dai Tokens from user's MetaMask Wallet to this smart contract.
-	
-		transferFrom is a delegite way to transfer assets on behalf of the "investor"
-		msg is a global variable in Solidity and corresponds to the message be sent whenever the stakeTokens functions is called
-	    sender is the person who calls the function
-	    address(this) where this corresponds to the TokenFarm solidity contract -> then we cast the constract into an address
-		ex.
-			msg.sender is a message sent by the investor  (from)
-			address(this) is the address of the TokenFarm (to)
-	*/
-	function stakeTokens (uint _amount) public {
-		// Require amount for staking is greater than 0
-		require(_amount > 0 , "amount cannot be 0");
+		// Trasnfer Mock Dai tokens to this contract for staking
+		daiToken.transferFrom(msg.sender, address(this), _amount);
 
-		// Transfer Mock Dai tokens to this contract for staking
-		daiToken.transferFrom(msg.sender,address(this), _amount); // address(this) is the App
-
-		// Update Staking Balance
+		// Update staking balance
 		stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-		// Add users to stakers array *only* if they haven't staked already
-		if(!hasStaked[msg.sender]){
+		// Add user to stakers array *only* if they haven't staked already
+		if(!hasStaked[msg.sender]) {
 			stakers.push(msg.sender);
 		}
 
 		// Update staking status
 		isStaking[msg.sender] = true;
 		hasStaked[msg.sender] = true;
-
 	}
 
-	// 2. Issuing Tokens
-
-	// Give the same amount of Dapp for every Dai token staked
-	function issueTokens () public {
-		// Only owner can call this function
-		require (msg.sender == owner, "caller must be the owner");
-
-		// Issue tokens to all stakers
-		for (uint i = 0; i < stakers.length; i++){
-			address recipient = stakers[i];
-			uint balance = stakingBalance[recipient];
-			if(balance > 0 ){
-			dappToken.transfer(recipient,balance);
-		    } 
-
-		}
-	}
-
-
-	// 3. User Unstakes Tokens (Withdraw)
-
+	// Unstaking Tokens (Withdraw)
 	function unstakeTokens() public {
-
-		// Fetch amount balance
+		// Fetch staking balance
 		uint balance = stakingBalance[msg.sender];
-		// Require balance greater than 0
-		require(balance > 0 , "balance cannot be less or equal to 0");
+
+		// Require amount greater than 0
+		require(balance > 0, "staking balance cannot be 0");
+
+		// Transfer Mock Dai tokens to this contract for staking
 		daiToken.transfer(msg.sender, balance);
 
+		// Reset staking balance
 		stakingBalance[msg.sender] = 0;
 
+		// Update staking status
 		isStaking[msg.sender] = false;
-
 	}
-	
 
+	// Issuing Tokens
+	function issueTokens() public {
+		// Only owner can call this function
+		require(msg.sender == owner, "caller must be the owner");
 
+		// Issue tokens to all stakers
+		for (uint i=0; i<stakers.length; i++) {
+			address recipient = stakers[i];
+			uint balance = stakingBalance[recipient];
+			if(balance > 0) {
+				dappToken.transfer(recipient, balance);
+			}
+		}
+	}
 }
