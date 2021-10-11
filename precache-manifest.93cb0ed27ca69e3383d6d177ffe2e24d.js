b11271cd701b846675514193ccd3331cf1aefcfb
vvittis <vasilisvittis@gmail.com>
2021-10-11 22:49:59 +0300

    Updates

---
+++ precache-manifest.93cb0ed27ca69e3383d6d177ffe2e24d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fe5f6f2c6787924b01d2ad47d740f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f51ff593e6a8ddec83",
+    "url": "/static/css/2.ffe872a2.chunk.css"
+  },
+  {
+    "revision": "adb0a8d38f74a6d8a3a0",
+    "url": "/static/css/main.59ded421.chunk.css"
+  },
+  {
+    "revision": "b4f51ff593e6a8ddec83",
+    "url": "/static/js/2.4b3e398d.chunk.js"
+  },
+  {
+    "revision": "adb0a8d38f74a6d8a3a0",
+    "url": "/static/js/main.d400f7d2.chunk.js"
+  },
+  {
+    "revision": "a55a9ef8e178661a4885",
+    "url": "/static/js/runtime-main.be9804ee.js"
+  },
+  {
+    "revision": "cfebf46feeaa9bbd5393c58bb21e5127",
+    "url": "/static/media/banner_background.cfebf46f.png"
+  },
+  {
+    "revision": "e8d8ebd2b2a6d9b2006135cfffad5210",
+    "url": "/static/media/customfont.e8d8ebd2.otf"
+  }
+]);
